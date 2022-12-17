fd544cd56cba442a7926f97570e6d9209fb441fc
IvanNikoleav <nik.ivan.mi@gmail.com>
2022-12-18 02:25:23 +0300

    add User model

--- src/app/settings.py
+++ src/app/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "web",
 ]
 
 MIDDLEWARE = [
@@ -118,6 +119,7 @@ USE_I18N = True
 USE_TZ = True
 
 
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
@@ -127,3 +129,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = 'web.User'
--- src/web/models.py
+++ src/web/models.py
@@ -1,4 +1,34 @@
 from django.db import models
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
 
-# Create your models here.
+from app.enums import Role
+
+
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class User(BaseModel, AbstractBaseUser, PermissionsMixin):
+    name = models.CharField(max_length=128, null=True)
+    surname = models.CharField(max_length=128, null=True)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=128)
+    role = models.CharField(
+        choices=Role.choices,
+        max_length=15,
+        default=Role.user
+    )
+
+    @property
+    def is_superuser(self):
+        return self.role == Role.admin
+
+    EMAIL_FIELD = "email"
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
 
