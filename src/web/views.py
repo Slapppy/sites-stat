094206793b4b8d3c8dfbc035a5f2c4a372bf8fdf
Dasha <dasha.ved93@gmail.com>
2022-12-19 23:45:30 +0300

    + profile view with search

--- src/web/views.py
+++ src/web/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+from web.models import Counter
+
+
+def profile_view(request):
+    search = request.GET.get("search", None)
+    counters = Counter.objects.all()
+
+    if search:
+        counters = counters.filter(name__icontains=search)
+
+    return render(request, "web/profile.html", {"counters": counters})
