a93c485fd6a123bb2608c9de7f9c7c7085770382
Dasha <dasha.ved93@gmail.com>
2022-12-19 23:44:33 +0300

    + profile design

--- src/app/urls.py
+++ src/app/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from web.views import profile_view
 
 urlpatterns = [
+    path("profile/", profile_view, name="profile"),
     path("admin/", admin.site.urls),
 ]
