1fd3a36e65341612080003dfd8c424018a3b28a6
linar <chevianchik@gmail.com>
2022-12-22 20:48:55 +0300

    + add

--- src/web/urls.py
+++ src/web/urls.py
@@ -1,12 +1,12 @@
-from django.urls import path
+from django.contrib import admin
+from django.urls import path, include
 from . import views
+from .views import CounterCreate
 
 urlpatterns = [
-
-    #path('', views.main_page, name='main_page'),
+    path("admin/", admin.site.urls),
     path('auth', views.auth_page, name='auth'),
     path('registration', views.registration_page, name='registration'),
-
-
+    path('add', CounterCreate.as_view()),
 
 ]
