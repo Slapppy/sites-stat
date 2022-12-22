2a976d97bb9c4fb8e0de4b994e772e165967e45c
linar <chevianchik@gmail.com>
2022-12-22 20:47:50 +0300

    + AddCounterForm

---
+++ src/web/forms.py
@@ -0,0 +1,26 @@
+from django import forms
+from .models import Counter
+from django.contrib.auth.forms import UserCreationForm
+from django.forms import ModelForm
+from .models import User
+
+
+class AddCounterForm(forms.ModelForm):
+
+    class Meta:
+        model = Counter
+        fields = ['name', 'link']
+
+
+
+
+class CreateUserForm(UserCreationForm):
+    name = forms.CharField(max_length=40)
+    surname = forms.CharField(max_length=50)
+    email = forms.EmailField(max_length=70)
+    password1 = forms.CharField(widget=forms.PasswordInput)
+    password2 = forms.CharField(widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ['name', 'surname', 'email'  ]
