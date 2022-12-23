319756fb9dda4c9abc3fa050d61909647872a53e
maratahmetov <ax-marat@mail.ru>
2022-12-23 16:25:02 +0300

    +add urls in profile

--- src/web/urls.py
+++ src/web/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path("auth", views.auth_page, name="auth"),
     path("registration", views.registration_page, name="registration"),
     path("profile", views.CountersListView.as_view(), name="profile"),
-    path("add", CounterCreate.as_view()),
+    path("add", CounterCreate.as_view(), name = 'add'),
     path("", views.main_page, name="main"),
 ]
