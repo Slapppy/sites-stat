e7513c5d6d8983fe55c6059a07dc47584dcfefe2
IvanNikoleav <nik.ivan.mi@gmail.com>
2022-12-28 22:18:58 +0300

    + clickhouse model

---
+++ src/web/clickhouse_models.py
@@ -0,0 +1,18 @@
+from infi.clickhouse_orm import Database, Model, DateTimeField, UInt16Field, UUIDField, StringField, FixedStringField, \
+    MergeTree
+
+
+class Views(Model):
+    counter_id = UInt16Field()
+    view_id = UUIDField()
+    visitor_unique_key = UUIDField()
+    referer = StringField()
+    device_type = StringField()
+    browser_type = StringField()
+    user_agent = StringField()
+    os_type = StringField()
+    ip = FixedStringField(16)
+    language = FixedStringField(2)
+    created_at = DateTimeField()
+
+    engine = MergeTree("created_at", ("created_at",))
