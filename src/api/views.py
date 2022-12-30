887edec34e2ff7aa766a7c534ca11f90ddce958a
Dasha <dasha.ved93@gmail.com>
2022-12-31 01:56:29 +0300

    = filter views with lookup

--- src/api/views.py
+++ src/api/views.py
@@ -8,19 +8,19 @@ from web.clickhouse_models import Views
 
 
 class StatCounterView(APIView):
-    def get_data(self, counter_id, start_date, end_date):
+    @staticmethod
+    def get_data(counter_id, start_date, end_date):
         db = create_connection()
-        views = Views.objects_in(db).filter(
-            (Views.counter_id == counter_id) & (Views.created_at >= start_date) & (Views.created_at < end_date)
-        )
+        views = Views.objects_in(db).filter(created_at__between=(start_date, end_date), counter_id=counter_id)
         return views
 
     def get(self, request):
         counter_id, date = request.GET.get("id", None), request.GET.get("date", None)
+
         if counter_id and date:
             start_date = datetime.strptime(date, "%Y-%m-%d")
-            end_date = start_date + timedelta(days=1)
+            end_date = start_date + timedelta(days=1, milliseconds=-1)
             views = self.get_data(counter_id, start_date, end_date)
             return Response({"counter_id": int(counter_id), "date": date, "count_views": views.count()})
-        response = {"error": [{"code": 400, "reason": "invalidParameter"}]}
-        return Response(response, status=status.HTTP_400_BAD_REQUEST)
+
+        return Response({"error": [{"code": 400, "reason": "invalidParameter"}]}, status=status.HTTP_400_BAD_REQUEST)
