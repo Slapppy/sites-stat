19a087fc0751bc25d2a473355fcd0c3454cf2650
maratahmetov <ax-marat@mail.ru>
2023-01-06 22:52:18 +0300

    +get data from pixel

--- src/api/views.py
+++ src/api/views.py
@@ -1,8 +1,10 @@
+from django.http import HttpResponse
+import uuid
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from datetime import datetime, timedelta
-
+import httpagentparser
 from app.clickhouse import create_connection
 from web.clickhouse_models import Views
 
@@ -34,3 +36,21 @@ class StatCounterView(APIView):
             )
 
         return Response({"error": [{"code": 400, "reason": "invalidParameter"}]}, status=status.HTTP_400_BAD_REQUEST)
+
+
+TRANSPARENT_1_PIXEL_GIF = b"\x47\x49\x46\x38\x39\x61\x01\x00\x01\x00\x80\x00\x00\xff\xff\xff\x00\x00\x00\x21\xf9\x04\x01\x00\x00\x00\x00\x2c\x00\x00\x00\x00\x01\x00\x01\x00\x00\x02\x02\x44\x01\x00\x3b"
+
+class GetMetaDataView(APIView):
+
+    def get(self, request , id):
+        db = create_connection()
+        metadata = request.headers['User-Agent']
+        data_split = httpagentparser.detect(metadata, 'os')
+        referer = request.META.get('HTTP_REFERER')
+        browser = data_split['browser']['name']
+        os_type = data_split['platform']['name']
+        device_type = data_split['os']['name']
+        ip_address = request.META['REMOTE_ADDR']
+        language = request.META['HTTP_ACCEPT_LANGUAGE'].split(',')[0][:2]
+
+        return HttpResponse(TRANSPARENT_1_PIXEL_GIF, content_type='image/gif')
