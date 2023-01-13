f74bb1c4a43c651cb03658043be74a5a43c0b1b8
Radmir Salimov <radmir-prog@mail.ru>
2023-01-13 22:20:33 +0300

    + path to counter view

--- src/web/urls.py
+++ src/web/urls.py
@@ -1,13 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
 from . import views
-from .views import CounterCreate, RegistrationView
+from .views import CounterCreate, RegistrationView, CounterDetailView
 
 urlpatterns = [
     path("auth", views.auth_page, name="auth"),
     path("registration", RegistrationView.as_view(), name="registration"),
     path("profile", views.CountersListView.as_view(), name="profile"),
     path("add", CounterCreate.as_view(), name="add"),
-    path("profile/counter/<int:id>", views.counter_page(), name="counter"),
+    path("profile/counter/<int:pk>", CounterDetailView.as_view(), name="counter"),
     path("", views.main_page, name="main"),
 ]
