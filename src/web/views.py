a368ee715245f099e42dfb7440d1dbf87e0fc359
DashaVed <dasha.ved93@gmail.com>
2023-02-15 15:25:13 +0300

    = change import reverse function

--- src/web/views.py
+++ src/web/views.py
@@ -1,7 +1,8 @@
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, redirect, get_object_or_404, reverse
+from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
+from django.urls import reverse
 from django.utils.decorators import method_decorator
 from django.views.generic import ListView, CreateView, View, DetailView, UpdateView
 
