ee3b7c7cd30f26c1ed3b5bb78dc7da72a13a16bd
maratahmetov <ax-marat@mail.ru>
2023-02-18 21:20:02 +0300

    + add logout

--- src/web/urls.py
+++ src/web/urls.py
@@ -1,5 +1,5 @@
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, reverse_lazy
+from django.contrib.auth.views import LogoutView
 from . import views
 from .views import CounterCreate, RegistrationView, CounterDetailView, CounterEditView, CounterDeleteView
 
@@ -11,5 +11,6 @@ urlpatterns = [
     path("edit/<int:id>", CounterEditView.as_view(), name="edit"),
     path("profile/counter/<int:pk>", CounterDetailView.as_view(), name="counter"),
     path("counter/<int:pk>/delete/", CounterDeleteView.as_view(), name="counter_delete"),
+    path("logout/", LogoutView.as_view(next_page=reverse_lazy("main")), name="logout"),
     path("", views.main_page, name="main"),
 ]
