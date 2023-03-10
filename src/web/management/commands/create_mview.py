d952e68ecdae8faf533417892abd8c00ebd8662d
linar <chevianchik@gmail.com>
2023-03-10 14:57:07 +0300

    + materialazed view for count views

--- src/web/management/commands/create_mview.py
+++ src/web/management/commands/create_mview.py
@@ -9,6 +9,7 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         db = create_connection()
         try:
+
             db.raw(
                 "CREATE MATERIALIZED VIEW visitor_in_day_mv TO visitorinday AS SELECT counter_id, "
                 "count(visitor_unique_key) as count_visitor, toDate(created_at) as created_at FROM views "
@@ -18,6 +19,16 @@ class Command(BaseCommand):
                 "INSERT INTO visitorinday SELECT counter_id, count(visitor_unique_key) as count_visitor, "
                 "toDate(created_at) as created_at FROM views GROUP BY counter_id, created_at;"
             )
+            db.raw(
+                "CREATE MATERIALIZED VIEW view_in_day_mv TO viewinday AS SELECT counter_id, "
+                "count(view_id) as count_visits, toDate(created_at) as created_at FROM views "
+                "GROUP BY counter_id, created_at;"
+            )
+            db.raw(
+                "INSERT INTO viewinday SELECT counter_id, count(view_id) as count_visits, "
+                "toDate(created_at) as created_at FROM views GROUP BY counter_id, created_at;"
+            )
+            print("Command completed successfully")
+
         except ServerError:
-            pass
-        print("command completed successfully")
+            print("View creation error")
