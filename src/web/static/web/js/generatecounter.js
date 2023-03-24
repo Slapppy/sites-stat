d4c2dc8ebb0852ddb3ab6f31009f1fd92b81b356
linar <chevianchik@gmail.com>
2023-03-24 19:41:40 +0300

    + generate jss script for take data

---
+++ src/web/static/web/js/generatecounter.js
@@ -0,0 +1,14 @@
+var matrika = document.getElementById("text");
+const data = document.currentScript.dataset;
+const idCounter = data.idCounter
+
+function innerTextScript(idCounter) {
+  matrika.innerText = `
+      <noscript><img></noscript>
+<script src="http://127.0.0.1:8000/api/getmetadata/${idCounter}"/"></script>
+<script>init(100)</script>`;
+};
+
+
+
+innerTextScript(idCounter);
