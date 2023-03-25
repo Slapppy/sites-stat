e5c23125d57dca3d437dc1cfb73b3866719d797e
DashaVed <dasha.ved93@gmail.com>
2023-03-25 17:11:00 +0300

    = date selector

--- src/web/static/web/frontend/src/containers/CounterPage.vue
+++ src/web/static/web/frontend/src/containers/CounterPage.vue
@@ -1,19 +1,44 @@
 <template>
-  <div class="filter-wrapper">
-      <div class="filter-panel">
-        <button id="btn3Days" class="filter-btn" :class="{ active: filter === 'threedays' }" @click="handleFilterClick('threedays')">3 дня</button>
-        <button id="btnWeek" class="filter-btn" :class="{ active: filter === 'week' }" @click="handleFilterClick('week')">Неделя</button>
-        <button id="btnMonth" class="filter-btn" :class="{ active: filter === 'month' }" @click="handleFilterClick('month')">Месяц</button>
-        <button id="btnQuarter" class="filter-btn" :class="{ active: filter === 'quarter' }" @click="handleFilterClick('quarter')">Квартал</button>
-        <button id="btnYear" class="filter-btn" :class="{ active: filter === 'year' }" @click="handleFilterClick('year')">Год</button>
+  <div class="page-charts">
+    <div class="filter-wrapper">
+      <div class="date-selector mb-3">
+        <span class="date-radio-buttons">
+            <label class="radio-button">
+                <input class="input-radio-button" type="radio" name="period" value="threedays"
+                       @click="handleFilterClick('threedays')">
+                <span class="title-radio-button left-radio-button">3 дня</span>
+            </label>
+            <label class="radio-button">
+                <input class="input-radio-button" type="radio" name="period" value="week"
+                       @click="handleFilterClick('week')">
+                <span class="title-radio-button">Неделя</span>
+            </label>
+            <label class="radio-button">
+                <input class="input-radio-button" type="radio" name="period" value="month"
+                       @click="handleFilterClick('month')" checked>
+                <span class="title-radio-button">Месяц</span>
+            </label>
+            <label class="radio-button">
+                <input class="input-radio-button" type="radio" name="period" value="quarter"
+                       @click="handleFilterClick('quarter')">
+                <span class="title-radio-button">Квартал</span>
+            </label>
+            <label class="radio-button">
+                <input class="input-radio-button" type="radio" name="period" value="year"
+                       @click="handleFilterClick('year')">
+                <span class="title-radio-button  right-radio-button">Год</span>
+            </label>
+        </span>
       </div>
-</div>
-
-  <div class="chart-container" id="chart-container"></div>
-  <div class="visit-chart" id="visit-chart"></div>
-  <div class="visitor-chart" id="visitor-chart"></div>
-
+    </div>
+    <div class="charts">
+      <div class="view-chart" id="view-chart"></div>
+      <div class="visit-chart" id="visit-chart"></div>
+      <div class="visitor-chart" id="visitor-chart"></div>
+    </div>
+  </div>
 </template>
+
 <script>
 import Highcharts from 'highcharts'
 import $ from 'jquery'
@@ -34,7 +59,7 @@ export default {
   },
   methods: {
     loadData() {
-       const currentUrl = window.location.href;
+      const currentUrl = window.location.href;
       const url = new URL(currentUrl);
       const path = url.pathname.slice(1);
       const id = path.split('/')[1];
@@ -80,7 +105,7 @@ export default {
     },
     updateChartViews() {
       const chartData = this.apiResponseViews.data.map(item => [Date.parse(item.date), item.count_views]);
-      Highcharts.chart('chart-container', {
+      Highcharts.chart('view-chart', {
         chart: {
           type: 'line'
         },
@@ -109,24 +134,24 @@ export default {
           positioner: function (labelWidth, labelHeight, point) {
             var tooltipX, tooltipY;
             if (point.plotX + labelWidth > this.chart.plotWidth) {
-                tooltipX = point.plotX + this.chart.plotLeft - labelWidth - 10;
+              tooltipX = point.plotX + this.chart.plotLeft - labelWidth - 10;
             } else {
-                tooltipX = point.plotX + this.chart.plotLeft + 10;
+              tooltipX = point.plotX + this.chart.plotLeft + 10;
             }
             tooltipY = point.plotY + this.chart.plotTop - labelHeight - 10;
             return {
-                x: tooltipX,
-                y: tooltipY
+              x: tooltipX,
+              y: tooltipY
             };
           }
         },
- plotOptions: {
-    series: {
-      softThreshold: true // включаем опцию softThreshold для более плавных изломов
-    }
-  },
+        plotOptions: {
+          series: {
+            softThreshold: true // включаем опцию softThreshold для более плавных изломов
+          }
+        },
         series: [{
-          name: 'Посещения',
+          name: 'Просмотры',
           data: chartData
         }]
       });
@@ -162,22 +187,22 @@ export default {
           positioner: function (labelWidth, labelHeight, point) {
             var tooltipX, tooltipY;
             if (point.plotX + labelWidth > this.chart.plotWidth) {
-                tooltipX = point.plotX + this.chart.plotLeft - labelWidth - 10;
+              tooltipX = point.plotX + this.chart.plotLeft - labelWidth - 10;
             } else {
-                tooltipX = point.plotX + this.chart.plotLeft + 10;
+              tooltipX = point.plotX + this.chart.plotLeft + 10;
             }
             tooltipY = point.plotY + this.chart.plotTop - labelHeight - 10;
             return {
-                x: tooltipX,
-                y: tooltipY
+              x: tooltipX,
+              y: tooltipY
             };
           }
         },
- plotOptions: {
-    series: {
-      softThreshold: true // включаем опцию softThreshold для более плавных изломов
-    }
-  },
+        plotOptions: {
+          series: {
+            softThreshold: true // включаем опцию softThreshold для более плавных изломов
+          }
+        },
         series: [{
           name: 'Посещения',
           data: chartData
@@ -215,24 +240,24 @@ export default {
           positioner: function (labelWidth, labelHeight, point) {
             var tooltipX, tooltipY;
             if (point.plotX + labelWidth > this.chart.plotWidth) {
-                tooltipX = point.plotX + this.chart.plotLeft - labelWidth - 10;
+              tooltipX = point.plotX + this.chart.plotLeft - labelWidth - 10;
             } else {
-                tooltipX = point.plotX + this.chart.plotLeft + 10;
+              tooltipX = point.plotX + this.chart.plotLeft + 10;
             }
             tooltipY = point.plotY + this.chart.plotTop - labelHeight - 10;
             return {
-                x: tooltipX,
-                y: tooltipY
+              x: tooltipX,
+              y: tooltipY
             };
           }
         },
- plotOptions: {
-    series: {
-      softThreshold: true // включаем опцию softThreshold для более плавных изломов
-    }
-  },
+        plotOptions: {
+          series: {
+            softThreshold: true // включаем опцию softThreshold для более плавных изломов
+          }
+        },
         series: [{
-          name: 'Посещения',
+          name: 'Посетители',
           data: chartData
         }]
       });
@@ -241,13 +266,12 @@ export default {
 
     handleFilterClick(filter) {
       this.filter = filter;
-      console.log(filter)
       this.loadData();
     },
 
-    },
-        }
-  </script>
+  },
+}
+</script>
 
 
 <style scoped>
@@ -255,82 +279,62 @@ export default {
 .filter-wrapper {
   margin-bottom: 20px;
   border-radius: 5px;
-  padding-left: 50px;
   position: relative;
   width: fit-content;
 }
-.filter-wrapper::before{
-  content: "";
-  position: absolute;
-  bottom: -20px;
-  width: 354%;
-  left: -10px;
-  height: 2px;
-  background: black;
-}
 
-.filter-panel {
-  display: flex;
-  justify-content: flex-end;
-  margin-bottom: 5px;
-  border: none;
-}
-
-.filter-btn {
-  border: 2px solid #4dcc40;
-  background-color: transparent;
-  color: #555;
+.radio-button {
   cursor: pointer;
-  font-size: 14px;
-  font-weight: bold;
-  padding: 5px 10px;
-  text-transform: uppercase;
-  text-align: center;
-  margin-left: -2px;
 }
 
-.filter-btn.active {
-  color: #4dcc40;
-  border-bottom: 2px solid #4dcc40;
-
-}
-
-.filter-btn:first-child {
-  border-right: none;
+.radio-button:first-child {
   margin-left: 0;
 }
 
-.filter-btn:last-child {
-  border-left: none;
-  margin-right: 0;
+.input-radio-button {
+  width: auto;
+  height: 100%;
+  appearance: none;
+  outline: none;
 }
 
-.chart-container{
-    margin-top:70px;
-    width: 450px;
-    height: 250px;
-      border: 1px solid black;
-      margin-left: 20px;
+.title-radio-button {
+  border: 1px solid #B8B5B5;
+  margin-left: -.13em;
+  font-size: 14px;
+  padding: 4px 14px 4px 14px;
+  text-align: center;
+}
 
-  }
+.title-radio-button:hover {
+  border: 1px solid #8a8787;
+}
 
-.visit-chart{
-    margin-top:-250px;
-    width: 450px;
-    height: 250px;
-    border: 1px solid black;
-    margin-left: 493px;
+.input-radio-button:checked + .title-radio-button {
+  background-color: #C9FF55;
+}
 
-  }
+.left-radio-button {
+  border-top-left-radius: 3px;
+  border-bottom-left-radius: 3px;
+}
 
-.visitor-chart{
-    margin-top:-250px;
-    width: 450px;
-    height: 250px;
-      border: 1px solid black;
-      margin-left: 966px;
+.right-radio-button {
+  border-top-right-radius: 3px;
+  border-bottom-right-radius: 3px;
+}
 
-  }
+.charts {
+  display: flex;
+  width: 100%;
+}
+
+.view-chart, .visit-chart, .visitor-chart {
+  margin-right: 10px;
+  width: 450px;
+  height: 250px;
+  border: 1px solid black;
+}
 
 
 </style>
