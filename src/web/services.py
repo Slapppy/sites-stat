526cfe164da4608ea0aea628e95a472c5c6217b9
maratahmetov <ax-marat@mail.ru>
2023-05-26 19:17:28 +0300

    +check null counters

--- src/web/services.py
+++ src/web/services.py
@@ -23,23 +23,24 @@ def add_parameters_into_counters(counters):
     counters_with_params = [
         {"id": counter_id, "count_views": 0, "count_visits": 0, "count_visitors": 0} for counter_id in counters_id
     ]
-    for raw in views:
-        counter_with_params = list(filter(lambda x: x["id"] == raw.counter_id, counters_with_params))
-        counter_with_params[0]["count_views"] += raw.count_views
+    if len(counters_with_params) > 0:
+        for raw in views:
+            counter_with_params = list(filter(lambda x: x["id"] == raw.counter_id, counters_with_params))
+            counter_with_params[0]["count_views"] += raw.count_views
 
-    for raw in visits:
-        counter_with_params = list(filter(lambda x: x["id"] == raw.counter_id, counters_with_params))
-        counter_with_params[0]["count_visits"] += raw.count_visits
+        for raw in visits:
+            counter_with_params = list(filter(lambda x: x["id"] == raw.counter_id, counters_with_params))
+            counter_with_params[0]["count_visits"] += raw.count_visits
 
-    for raw in visitors:
-        counter_with_params = list(filter(lambda x: x["id"] == raw.counter_id, counters_with_params))
-        counter_with_params[0]["count_visitors"] += raw.count_visitors
+        for raw in visitors:
+            counter_with_params = list(filter(lambda x: x["id"] == raw.counter_id, counters_with_params))
+            counter_with_params[0]["count_visitors"] += raw.count_visitors
 
-    for counter in counters:
-        counter_with_params = list(filter(lambda x: x["id"] == counter.id, counters_with_params))[0]
-        counter.count_views = counter_with_params["count_views"]
-        counter.count_visits = counter_with_params["count_visits"]
-        counter.count_visitors = counter_with_params["count_visitors"]
+        for counter in counters:
+            counter_with_params = list(filter(lambda x: x["id"] == counter.id, counters_with_params))[0]
+            counter.count_views = counter_with_params["count_views"]
+            counter.count_visits = counter_with_params["count_visits"]
+            counter.count_visitors = counter_with_params["count_visitors"]
 
 
 def group_clickhouse_tables():
