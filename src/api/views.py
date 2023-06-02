768b39eafb535705714f8424c4396c34477d31c0
Usmanovlinar <chevianchik@gmail.com>
2023-06-02 21:56:37 +0300

    = HTTP_HOST

--- src/api/views.py
+++ src/api/views.py
@@ -93,7 +93,7 @@ class GetMetaDataView(APIView):
 
         metadata = request.headers["User-Agent"]
         data_split = httpagentparser.detect(metadata, "os")
-        referer = request.META["REMOTE_ADDR"]
+        referer = request.META["HTTP_HOST"]
         browser = data_split["browser"]["name"]
         os_type = data_split["platform"]["name"]
         device_type = data_split["os"]["name"]
