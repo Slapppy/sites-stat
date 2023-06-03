f81433aecfdb2f05ff7ebdf63381bdaa9d08c119
DashaVed <dasha.ved93@gmail.com>
2023-06-03 16:18:19 +0300

    + message when script are copied

--- src/frontend/src/views/CounterEdit/App.vue
+++ src/frontend/src/views/CounterEdit/App.vue
@@ -2,16 +2,19 @@
   <div ref="counterEdit" class="mt-5 mb-5">
     <div class="d-flex flex-wrap justify-content-between align-items-center mb-3" style="max-width: 600px;">
       <h3>Код счетчика</h3>
-      <button @click="copyText" class="btn-save">Копировать</button>
+      <div class="d-flex flex-wrap align-items-center">
+        <div v-if="message" class="me-3">{{ message }}</div>
+        <button @click="copyScript" class="btn-save">Копировать</button>
+      </div>
     </div>
     <p ref="script" id="text" class="counter-code p-4">
       &lt;!-- FreeStat counter --&gt;
       <br>
       &lt;div id="counter_id" style="transform: translateX(9999px);"&gt;{{ counterId }}&lt;/div&gt;
       <br>
-      &lt;script src="https://{{ domain }}/src/src/assets/collectdata.js"&gt;&lt;/script&gt;
+      &lt;script src="https://site-stats.ya.uenv.ru/src/src/assets/collectdata.js"&gt;&lt;/script&gt;
       <br>
-      &lt;noscript&gt;&lt;div&gt;&lt;img src="https://{{ domain }}/api/getmetadata/{{ counterId }}"/&gt;&lt;/div&gt;&lt;/noscript&gt;
+      &lt;noscript&gt;&lt;div&gt;&lt;img src="https://site-stats.ya.uenv.ru/api/getmetadata/{{ counterId }}"/&gt;&lt;/div&gt;&lt;/noscript&gt;
     </p>
   </div>
 </template>
@@ -23,22 +26,21 @@ export default {
   data() {
     return {
       counterId: null,
-      domain: window.location.hostname,
+      message: ''
     }
   },
   created() {
     this.counterId = document.querySelector('#counter_edit').getAttribute('data-counter-id');
   },
-  setup() {
-    const script = ref(null);
-
-    const copyText = () => {
-      navigator.clipboard.writeText(script.value.innerText);
-      alert('Success')
-    };
-
-    return {script, copyText};
+  mounted() {
+    this.script = ref(null);
   },
+  methods: {
+    copyScript() {
+      navigator.clipboard.writeText(this.$refs.script.innerText);
+      this.message = 'Код скопирован!'
+    }
+  }
 }
 </script>
 
